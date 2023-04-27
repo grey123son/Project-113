30d35a4ec7c4849d6187663db319298980fbe024
grey123son <94587086+grey123son@users.noreply.github.com>
2023-04-26 17:53:19 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function preload(){
 
 function setup(){
     canvas = createCanvas(640, 480);
-    canvas.position(160, 250);
+    canvas.position(650, 250);
     video = createCapture(VIDEO);
     video.hide();
 
